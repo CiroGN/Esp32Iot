f3673ca739b376c2c1cb6a98a115f19dc98a06a4
CiroGN <ciroguilhermenass@gmail.com>
2025-06-04 16:36:09 -0300

    modificação do index e seu header no _layout

--- Esp32App/app/index.tsx
+++ Esp32App/app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
+  const styles = StyleSheet.create({
+    a:{ flex: 1,
         justifyContent: "center",
         alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+        flexWrap:"wrap",
+        flexDirection:"row",
+      },
+    title:{fontSize:20}
+  })
+  return (
+    <View style={styles.a}>
+      <View>
+        <Text style={styles.title}>Conexão das Esp's-32</Text>
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
