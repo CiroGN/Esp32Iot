1470a1a87422144bd9701c48e18ce8b6d6e74675
André <andre.justus@truework.com.br>
2025-06-11 17:23:48 -0300

    notifications e seen_tokens

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import hashlib
 import time
+import redis
+import json
+
+from bd import DatabaseConnection
 
 app = Flask(__name__)
 
@@ -10,11 +14,60 @@ def get_token():
     token_key_encode = (str(token_key)).encode('utf-8')
     token_key_hash = hashlib.sha256()
     token_key_hash.update(token_key_encode)
-    data = {
-        'status': 201,
-		'token_key': str(token_key_hash.hexdigest())
-    }
+    database = DatabaseConnection()
+    token = str(token_key_hash.hexdigest())
+    result = database.select(f"SELECT * FROM token WHERE token='{token}'")
+    if len(result) == 0:
+        database.query(f"INSERT INTO token (token) VALUES ('{token}')")
+        data = {
+            'status': 201,
+            'token_key': str(token_key_hash.hexdigest())
+        }
+        return jsonify(data)
+    else:
+        data = {
+            'status': 404
+        }
+        database.close()
+        return jsonify(data)
     
+@app.route('/get_notifications', methods=['GET'])
+def get_notifications():
+    redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
+    notifications = redis_client.lrange("notifications", 0, -1)
+    print(notifications)
+    notifications_json = [json.loads(item) for item in notifications]
+    data = {
+        "status": 200,
+        "notifications": notifications_json
+    }
     return jsonify(data)
+
+@app.route('/seen_token', methods=['POST'])
+def seen_token():
+    data = request.get_json()
+    token = data.get("token")
+    database = DatabaseConnection()
+    result = database.select(f"SELECT * FROM token WHERE token='{token}'")
+    if len(result) == 0:
+        redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
+        ts = int(time.time()*1000)
+        data_redis = {
+            'ts': ts,
+            'token_key': str(token)
+        }
+        redis_client.rpush("notifications", json.dumps(data_redis))
+        redis_client.close()
+        data = {
+            'status': 200
+        }
+        database.close()
+        return jsonify(data)
+    else:
+        data = {
+            'status': 404
+        }
+        database.close()
+        return jsonify(data)
 
 app.run(debug=True)
